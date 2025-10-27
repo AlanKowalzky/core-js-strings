ed6f20ea768f1e655609996906dcb28eb6d25359
AlanKowalzky <alan.kowalzky@gmail.com>
2025-10-28 00:21:50 +0100

    fix: Fix remaining JSDoc parameter errors

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -288,7 +288,7 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
+function countVowels(str) {
   throw new Error('Not implemented');
 }
 
@@ -305,7 +305,7 @@ function countVowels(/* str */) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
+function isPalindrome(str) {
   throw new Error('Not implemented');
 }
 
@@ -321,7 +321,7 @@ function isPalindrome(/* str */) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
+function findLongestWord(sentence) {
   throw new Error('Not implemented');
 }
 
@@ -335,7 +335,7 @@ function findLongestWord(/* sentence */) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
+function reverseWords(str) {
   throw new Error('Not implemented');
 }
 
@@ -350,7 +350,7 @@ function reverseWords(/* str */) {
  *   invertCase('JavaScript is Fun') => 'jAVAsCRIPT IS fUN'
  *   invertCase('12345') => '12345'
  */
-function invertCase(/* str */) {
+function invertCase(str) {
   throw new Error('Not implemented');
 }
 
@@ -367,7 +367,7 @@ function invertCase(/* str */) {
  *   getStringFromTemplate('John','Doe') => 'Hello, John Doe!'
  *   getStringFromTemplate('Chuck','Norris') => 'Hello, Chuck Norris!'
  */
-function getStringFromTemplate(/* firstName, lastName */) {
+function getStringFromTemplate(firstName, lastName) {
   throw new Error('Not implemented');
 }
 
@@ -381,7 +381,7 @@ function getStringFromTemplate(/* firstName, lastName */) {
  *   extractNameFromTemplate('Hello, John Doe!') => 'John Doe'
  *   extractNameFromTemplate('Hello, Chuck Norris!') => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
+function extractNameFromTemplate(value) {
   throw new Error('Not implemented');
 }
 
@@ -396,7 +396,7 @@ function extractNameFromTemplate(/* value */) {
  *   unbracketTag('<span>') => 'span'
  *   unbracketTag('<a>') => 'a'
  */
-function unbracketTag(/* str */) {
+function unbracketTag(str) {
   throw new Error('Not implemented');
 }
 
@@ -415,7 +415,7 @@ function unbracketTag(/* str */) {
  *   ],
  *   'info@gmail.com' => ['info@gmail.com']
  */
-function extractEmails(/* str */) {
+function extractEmails(str) {
   throw new Error('Not implemented');
 }
 
@@ -435,7 +435,7 @@ function extractEmails(/* str */) {
  *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
  *
  */
-function encodeToRot13(/* str */) {
+function encodeToRot13(str) {
   throw new Error('Not implemented');
 }
 
@@ -463,7 +463,7 @@ function encodeToRot13(/* str */) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
+function getCardId(value) {
   throw new Error('Not implemented');
 }
 
